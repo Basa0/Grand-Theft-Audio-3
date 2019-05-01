ddd10a013d6d936d941f7eac699cc98829e72212
Basa <basangeacalexandru@gmail.com>
2019-05-01 12:05:50 +0300

    Skippable mission intro

--- Assets/Scripts/IntroMission.cs
+++ Assets/Scripts/IntroMission.cs
@@ -15,28 +15,41 @@ public class IntroMission : MonoBehaviour {
 
 	void Start()
 	{
-		StartCoroutine("startIntro");
+		StartCoroutine("StartIntro");
 		PlayerPrefs.SetString("player", "orange");
 	}
 
-	IEnumerator startIntro()
-	{
-		yield return new WaitForSeconds(1f);
-		missionNameText.gameObject.SetActive(true);
-		yield return new WaitForSeconds(1f);
-		introText.text = "I know a place on the edge of the Red Light District where we can lay low,";
-		yield return new WaitForSeconds(5f);
-		introText.text = "but my hands are all messed up so you better drive, brother.";
-		yield return new WaitForSeconds(4f);
-		missionNameText.gameObject.SetActive(false);
-		yield return new WaitForSeconds(1f);
-		introText.text = "";
-		heightBall.gameObject.SetActive(false);
-		playerIntro.gameObject.SetActive(false);
-		blackBar.gameObject.SetActive(false);
-		introCamera.gameObject.SetActive(false);
-		introText.gameObject.SetActive(false);
-		player.gameObject.SetActive(true);
-		gameUI.gameObject.SetActive(true);
-	}
+    void Update()
+    {
+        if (Input.anyKey) {
+            StopCoroutine("StartIntro");
+            CleanupIntro();
+        }
+    }
+
+	IEnumerator StartIntro()
+    {
+        yield return new WaitForSeconds(1f);
+        missionNameText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        introText.text = "I know a place on the edge of the Red Light District where we can lay low,";
+        yield return new WaitForSeconds(5f);
+        introText.text = "but my hands are all messed up so you better drive, brother.";
+        yield return new WaitForSeconds(4f);
+        missionNameText.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        CleanupIntro();
+    }
+
+    private void CleanupIntro()
+    {
+        introText.text = "";
+        heightBall.gameObject.SetActive(false);
+        playerIntro.gameObject.SetActive(false);
+        blackBar.gameObject.SetActive(false);
+        introCamera.gameObject.SetActive(false);
+        introText.gameObject.SetActive(false);
+        player.gameObject.SetActive(true);
+        gameUI.gameObject.SetActive(true);
+    }
 }
